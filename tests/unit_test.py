5bbb8db727627dde704ea6e5c5fd9f5bb9c0ab8b
Leonard <leonard.fisser@tuhh.de>
2022-04-08 16:05:16 +0200

    Extend unit test code coverage

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -3,7 +3,6 @@
 import unittest
 import simplenc
 
-
 class TestMatrixOperators(unittest.TestCase):
     """Tests the matrix utilities necessary for the BinaryCoder."""
 
@@ -67,5 +66,76 @@ class TestMatrixOperators(unittest.TestCase):
         return tests
 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+class TestBinaryCoder(unittest.TestCase):
+    """Tests the BinaryCoder."""
+
+    def setUp(self):
+        pass
+
+    def test_coding(self):
+        tests = self.get_coding_test_cases()
+
+        for test in tests:
+            rng_seed = 1
+            # Encoder
+            num_symbols = len(test["inputs"][0][0])
+            num_bits_packet = len(test["inputs"][0][1])
+            encoder = simplenc.BinaryCoder(num_symbols, num_bits_packet, rng_seed)
+            for inputs in test["inputs"]:
+                encoder.consume_packet(inputs[0],inputs[1])
+            self.assertEqual(encoder.rank(),test["solution_rank"])
+            self.assertEqual(encoder.get_num_decoded(),test["solution_num_decoded"])
+            self.assertEqual(encoder.is_fully_decoded(),test["solution_is_fully_decoded"])
+            self.assertEqual(encoder.get_decoded_symbol(0),test["solution_first_decoded_packet"])
+            self.assertEqual(encoder.get_sys_coded_packet(0),test["solution_first_sys_encoded_packet"])
+
+            # Decoder
+            decoder = simplenc.BinaryCoder(num_symbols, num_bits_packet, rng_seed)
+            for _ in range(15): # TODO: Magic number
+                decoder.consume_packet(*(encoder.get_new_coded_packet()))
+            self.assertEqual(encoder.rank(),test["solution_rank"])
+            self.assertEqual(decoder.get_num_decoded(),test["solution_num_decoded"])
+            self.assertEqual(decoder.is_fully_decoded(),test["solution_is_fully_decoded"])
+
+            # Check encoder and decoder states
+            self.assertEqual(decoder.packet_vector,encoder.packet_vector)
+
+
+    def get_coding_test_cases(self):
+        tests = []
+
+        # An encoder with all symbols fully decoded should create packets for all symbosl
+        tests.append({"inputs":[ ([1, 0, 0], [1, 1, 1]), 
+                                 ([0, 1, 0], [1, 1, 0]),
+                                 ([0, 0, 1], [1, 0, 1])],
+                      "solution_num_decoded": 3,
+                      "solution_rank": 3,
+                      "solution_first_decoded_packet": [1, 1, 1],
+                      "solution_first_sys_encoded_packet": ([1, 0, 0],[1, 1, 1]),
+                      "solution_is_fully_decoded": True})
+
+        tests.append({"inputs":[ ([1, 1, 1], [1, 1, 1]), 
+                                 ([0, 1, 1], [1, 1, 0])],
+                      "solution_num_decoded": 1,
+                      "solution_rank": 2,
+                      "solution_first_decoded_packet": [0, 0, 1],
+                      "solution_first_sys_encoded_packet": ([1, 0, 0],[0, 0, 1]),
+                      "solution_is_fully_decoded": False})
+
+        tests.append({"inputs":[ ([1, 1, 1], [1, 1, 1]), 
+                                 ([0, 0, 1], [1, 1, 0])],
+                      "solution_num_decoded": 1,
+                      "solution_rank": 2,
+                      "solution_first_decoded_packet": None,
+                      "solution_first_sys_encoded_packet": (None, None),
+                      "solution_is_fully_decoded": False})
+
+        tests.append({"inputs":[ ([1, 1, 1], [1, 1, 1]), 
+                                 ([0, 0, 1], [1, 1, 0]),
+                                 ([0, 0, 1], [1, 1, 0])],
+                      "solution_num_decoded": 1,
+                      "solution_rank": 2,
+                      "solution_first_decoded_packet": None,
+                      "solution_first_sys_encoded_packet": (None, None),
+                      "solution_is_fully_decoded": False})
+        return tests
